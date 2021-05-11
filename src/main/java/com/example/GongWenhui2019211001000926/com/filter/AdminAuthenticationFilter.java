c8b1cad0c306b54217fe86e5bb8a2da56e1f28a3
2019211001000926GongWenhui <80186729+2019211001000926GongWenhui@users.noreply.github.com>
2021-05-11 22:47:04 +0800

    week-9

---
+++ src/main/java/com/example/GongWenhui2019211001000926/com/filter/AdminAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.GongWenhui.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/admin/*")
+public class AdminAuthenticationFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        boolean isLoggedIn = (session != null && session.getAttribute("user") != null);
+        System.out.println(isLoggedIn);
+        String loginURI = httpRequest.getContextPath() + "/admin/login";
+        boolean isLoginRequest = httpRequest.getRequestURI().equals(loginURI);
+        boolean isLoginPage = httpRequest.getRequestURI().endsWith("login");
+        if (isLoggedIn && (isLoginRequest || isLoginPage)) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/home");
+            dispatcher.forward(request,response);
+        } else if(isLoggedIn || isLoginRequest) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect(httpRequest.getContextPath()+ "/login");
+        }
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
