e7a4c391d52ff21a15ba193d7364a8ec41519550
2019211001000926GongWenhui <80186729+2019211001000926GongWenhui@users.noreply.github.com>
2021-06-08 23:47:21 +0800

    LAB1

--- src/main/java/com/example/GongWenhui2019211001000926/com/week6/demo/SearchServlet.java
+++ src/main/java/com/example/GongWenhui2019211001000926/com/week6/demo/SearchServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URLEncoder;
 
 @WebServlet(name = "SearchServlet", value="/search")
