6cfe6f1c3b1c0ac98bfda5872d5ef34018f3ad7d
2019211001000926GongWenhui <80186729+2019211001000926GongWenhui@users.noreply.github.com>
2021-06-16 00:33:11 +0800

    WEEK-15

--- src/main/java/com/example/GongWenhui2019211001000926/com/controller/controller/controller/ProductListServlet.java
+++ src/main/java/com/example/GongWenhui2019211001000926/com/controller/controller/controller/ProductListServlet.java
@@ -24,7 +24,7 @@ public class ProductListServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
